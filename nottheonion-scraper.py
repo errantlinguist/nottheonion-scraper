58cd514f0cc08b955e4eae10c4d12699d778fdc2
Todd Shore <shore@ukp.informatik.tu-darmstadt.de>
2016-08-09 14:52:17 +0200

    Made script runnable; Added error-handling logic; Now writing both HTML and stripped text to (separate) files

--- nottheonion-scraper.py
+++ nottheonion-scraper.py
@@ -4,43 +4,38 @@
 A script for getting a corpus of "Onion-like" albeit real news articles.
 '''
 
-import gzip
-import io
-import json
-import logging
 import os
 import sys
-import re
+import newspaper
+import random
 import requests
+import requests.auth
+import string
+import time
 
-from collections import deque
-from newspaper import Article
-from newspaper import news_pool
-from urllib.error import HTTPError
 from urllib.parse import urlsplit
 
 DEFAULT_REQUEST_CHARSET = "UTF-8"
 
 URL_FILENAME_TRANSLATION_TABLE = {ord(':') : '-', ord('/') : os.path.sep, ord('\\') : '-', ord('*') : '-', ord('?') : '-', ord('"') : '\'', ord('<') : '-', ord('>') : '-', ord('|') : '-', ord('\0') : '0', ord('.') : os.path.sep}
 
-def crawl_reddit(url):
-#	req = create_request(url)
-#	with urllib.request.urlopen(req) as response:
-#		result = scrape_reddit_thing_urls_from_response(response)
-#	return result
-	r = requests.get(url, headers={
-		"Accept-Charset" : DEFAULT_REQUEST_CHARSET,
-		"Accept" : "application/json",
-#		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
-		"User-Agent" : "ubuntu:nottheonion-scraper:0.0.1 (by /u/errantlinguist)"
-	})
+__AUTHOR_REDDIT_USERNAME = "errantlinguist"
+__CLIENT_ID = "nottheonion-scraper"
+__VERSION = "0.0.1"
+__WEBSITE = "https://github.com/errantlinguist/nottheonion-scraper"
 
-#def create_request(url):
-#	return urllib.request.Request(url, data=None, headers={
-#		"Accept-Charset" : DEFAULT_REQUEST_CHARSET,
-#		"Accept" : "application/json",
-#		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
-#	})
+'''
+See: https://github.com/reddit/reddit/wiki/API#user-content-rules
+'''
+__USER_AGENT_STR = "%(platform)s:%(app_id)s:%(version)s (by /u/%(reddit_username)s)" % {"platform" : sys.platform, "app_id" : __CLIENT_ID, "version" : __VERSION, "reddit_username" : __AUTHOR_REDDIT_USERNAME}
+
+
+class AuthData(object):
+	def __init__(self, auth_token_response):
+		self.json = auth_token_response.json()
+		self.token_type = self.json["token_type"]
+		self.access_token = self.json["access_token"]
+		self.auth_expiration_time = time.time() + int(self.json["expires_in"])
 
 def create_url_filename(url_str):
 	'''
@@ -52,57 +47,63 @@ def create_url_filename(url_str):
 	if result.endswith(os.path.sep):
 		result = result[:len(result) - len(os.path.sep)]
 	return result
-
-def save_pages(urls, outpath_prefix):
-	url_attempt_queue = deque((url, 0) for url in urls)
-	while url_attempt_queue:
-		url, attempts = url_attempt_queue.popleft()
+	
+def refresh_auth_token(refresh_token, auth):
+	post_data = {"grant_type": "refresh_token", "refresh_token" : refresh_token}
+	headers = {
+		"User-Agent" : __USER_AGENT_STR
+	}
+	return requests.post("https://www.reddit.com/api/v1/access_token", auth=auth, data=post_data, headers=headers)	
+	
+def retrieve_auth_token(auth):
+	post_data = {"grant_type": "client_credentials"}
+	headers = {
+		"User-Agent" : __USER_AGENT_STR
+	}
+	return requests.post("https://www.reddit.com/api/v1/access_token", auth=auth, data=post_data, headers=headers)
+	
+def save_pages(url_articles, outpath_prefix):
+	for url, article in url_articles.items():
 		outpath_infix = create_url_filename(url)
 		outpath = os.path.join(outpath_prefix, outpath_infix)
 		if os.path.exists(outpath):
 			print("File path \"%s\" already exists; Skipping." % outpath, file=sys.stderr)
 		else:
-			print("Requesting article \"%s\"." % url, file=sys.stderr)
-			req = create_request(url)
-			with urllib.request.urlopen(req) as response:
-				info = response.info()
-				charset = info.get_content_charset()
-				print("Charset: %s" % charset)
-				if not charset:
-					charset = DEFAULT_REQUEST_CHARSET
-				data_str = response.read().decode(charset)
-		
-				# After getting the response data, write it to file
+			#print("Downloading article \"%s\"." % url, file=sys.stderr)
+			article.download()
+			try:
+				article.parse()
+			except newspaper.ArticleException as e:
+				if article.is_downloaded:
+					raise e
+				else:
+					print("Downloading the article for URL \"%s\" was not successful; Trying to download again." % url, file=sys.stderr)
+					
+			# After getting the response data, write it to file
+			outdir = os.path.dirname(outpath)
+			if not os.path.exists(outdir):
+				os.makedirs(outdir)
+			with open(outpath, 'w') as outf:
+				outf.write(article.html)
 			
-				outdir = os.path.dirname(outpath)
-				if not os.path.exists(outdir):
-					os.makedirs(outdir)
-				with open(outpath, 'w') as outf:
-					outf.write(data_str)
-				print("%s > %s" %(url, outpath), file=sys.stderr)
+			# Write the stripped contents to a text file
+			with open(outpath + ".txt", 'w') as outf:
+				outf.write(article.text)
+				
+			print("%s > %s" %(url, outpath), file=sys.stderr)
+			
 					
 					
 		
 def scrape_reddit_thing_urls_from_response(response):
-	code = response.getcode()
-	info = response.info()
-	charset = info.get_content_charset()
-	if not charset:
-		charset = DEFAULT_REQUEST_CHARSET
-	data_str = response.read().decode(charset)
-	#data_str = response.readall().decode(charset)
-	#print(data_str)
-	json_objs = json.loads(data_str)
-	with io.StringIO(data_str) as str_buffer:
-		json_objs = json.load(str_buffer, encoding=charset)
-		
-	reddit_thing_urls = scrape_reddit_thing_urls(json_objs)
-	last_thing_name = json_objs["data"]["after"]
+	data = response.json()["data"]
+	reddit_thing_urls = scrape_reddit_thing_urls(data)
+	last_thing_name = data["after"]
 	return reddit_thing_urls, last_thing_name
 
-def scrape_reddit_thing_urls(json_objs):
-	data = json_objs["data"]
+def scrape_reddit_thing_urls(data):
 	children = data["children"]
+	#print("Processing %d child(ren)." % len(children))
 	for child in children:
 		child_data = child["data"]
 		child_name = child_data["name"]
@@ -112,30 +113,64 @@ def scrape_reddit_thing_urls(json_objs):
 			yield (child_name, url)
 		else:
 			print("Reddit thing named \"%s\" has no \"%s\" attribute." %(child_name, url_attr), file=sys.stderr)
-
+			
+		
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print("Usage: %s URL OUTDIR" % sys.argv[0], file=sys.stderr)
+	if len(sys.argv) != 3:
+		print("Usage: %s REDDIT_APP_SECRET OUTDIR" % sys.argv[0], file=sys.stderr)
 		sys.exit(64)
 	else:
-		papers = []
-		get_method_root = "https://www.reddit.com/r/nottheonion/.json?limit=100"
-		current_get_method = get_method_root
-		while current_get_method:
-			reddit_thing_urls, last_thing_name = crawl_reddit(current_get_method)
+		# Oauth2 authentication <https://github.com/reddit/reddit/wiki/OAuth2#user-content-authorization>
+		# https://github.com/reddit/reddit/wiki/OAuth2-Quick-Start-Example#user-content-python-example
+		secret = sys.argv[1]
+		auth=("_JNFnqor9ZT4mQ", secret)
+		auth_token_response = retrieve_auth_token(auth)
+		auth_token_response.raise_for_status()
+		auth_data = AuthData(auth_token_response)
 		
-			urls = (url for name, url in reddit_thing_urls)
-			articles = (Article(url=url, fetch_images=False, follow_meta_refresh=True) for url in urls)
-			papers.extend(articles)
-			count = len(papers)
-			
-			if not last_thing_name:
-				current_get_method = None
-			else:
-				current_get_method = get_method_root + "&count=" + str(count) + "&after=" + last_thing_name
+		
+		params = {"limit" : 100}
+		while auth_token_response:
+			url_articles = {}
+			if auth_data.auth_expiration_time <= time.time():
+				print("Refreshing authentication token.", file=sys.stderr)
+				auth_token_response = refresh_auth_token(auth_data.access_token, auth)
+				auth_token_response.raise_for_status()
+				auth_data = AuthData(auth_token_response)
 				
-			print("%d articles to get." % len(papers))
-#		outdir = sys.argv[1]
-#		save_pages(urls, outdir)
+			headers = {
+				"Accept" : "application/json",
+				"Accept-Charset" : DEFAULT_REQUEST_CHARSET,
+				"Authorization": auth_data.token_type + " " + auth_data.access_token,
+				"User-Agent": __USER_AGENT_STR}
+			next_page_response = requests.get("https://oauth.reddit.com/r/nottheonion/.json", headers=headers, params=params)
+			try:
+				next_page_response.raise_for_status()
+			except HTTPError:
+				if next_page_response.status_code == 403:
+					print("Refreshing authentication token.", file=sys.stderr)
+					auth_token_response = refresh_auth_token(auth_data.access_token, auth)
+					auth_token_response.raise_for_status()
+					auth_data = AuthData(auth_token_response)
+					
+				
+			reddit_thing_urls, last_thing_name = scrape_reddit_thing_urls_from_response(next_page_response)
+			urls = (url for name, url in reddit_thing_urls)
+			for url in urls:
+				#print("Adding URL \"%s\" to batch." % url, file=sys.stderr)
+				article = newspaper.Article(url, follow_meta_refresh=False, keep_article_html=True)
+				url_articles[url] = article
+			
+			print("Retrieving %d articles." % len(url_articles), file=sys.stderr)
+			outdir = sys.argv[2]
+			save_pages(url_articles, outdir)
+			
+			if last_thing_name:
+				params["count"] += len(url_articles)
+				params["after"] = last_thing_name
+			else:
+				break
+				
+		print("Retrieved %d articles in total." % params["count"], file=sys.stderr)
 		
 		
